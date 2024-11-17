cad3698f47b592b7e73d213244d8157ce55ca476
Frizzbay <wdcdoyler@gmail.com>
2024-11-17 18:10:42 +0000

    Network diagram visualization of task dependencies

---
+++ sql/queries/task_dependencies_view.sql
@@ -0,0 +1,10 @@
+/*
+
+Task Dependencies Query
+Purpose: Retrieves task dependency relationships for visualization
+Used by: Network diagram visualization of task dependencies
+
+This query joins the task_dependencies table with the tasks table
+to show how tasks are related to each other, including their statuses.
+
+*/
